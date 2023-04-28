6c43b11d51acd479407278a160114febe021f455
MarcosYH <marcosdeimos18@gmail.com>
2023-04-28 15:03:57 +0200

    add bootstrap and basic achitecture

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
